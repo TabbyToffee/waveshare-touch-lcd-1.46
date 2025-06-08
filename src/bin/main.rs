5eb0cc0bb76d10ec5ce0506dccbd998d2d241c6b
TabbyToffee <147265710+TabbyToffee@users.noreply.github.com>
2025-06-08 16:57:33 +0100

    RTC works, THE PINS WERE SWICHED

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,18 @@
 #![no_std]
 #![no_main]
 
+use esp_hal::gpio::rtc_io::LowPowerOutputOpenDrain;
+use esp_hal::gpio::{DriveMode, Flex, InputConfig, Level, Output, OutputConfig, Pull, RtcPin};
+use esp_hal::peripherals::Peripherals;
+use esp_hal::Blocking;
 use watch_playground::display;
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp_hal::clock::CpuClock;
+use esp_hal::{clock::CpuClock, gpio::Input};
 use esp_hal::timer::systimer::SystemTimer;
 use esp_hal::timer::timg::TimerGroup;
+use embedded_hal;
 // use fugit;
 
 use embedded_graphics::{
@@ -35,12 +40,17 @@ use esp_hal::{
     Async,
 };
 use esp_println::{dbg, println};
+use pcf8563::{self, PCF8563, DateTime};
+// use pcf85063a::{self, Control, DateTime};
 
 const TCA9554_ADDRESS: u8 = 0x20;
 const TCA9554_INPUT_REG: u8 = 0x00;
 const TCA9554_OUTPUT_REG: u8 = 0x01;
 const TCA9554_POLARITY_REG: u8 = 0x02;
 const TCA9554_CONFIG_REG: u8 = 0x03;
+
+// RTC
+const PCF85063_ADDRESS: u8 = 0x51;
 
 const ESP_PANEL_LCD_SPI_IO_TE: u8 = 18;
 const ESP_PANEL_LCD_SPI_IO_SCK: u8 = 40;
@@ -55,21 +65,26 @@ const EXAMPLE_LCD_PIN_NUM_BK_LIGHT: u8 = 5;
 const ESP_PANEL_LCD_SPI_CLK_MHZ: u32 = 80; // 80Mhz
 
 #[panic_handler]
-fn panic(_: &core::panic::PanicInfo) -> ! {
-    println!("Crashed");
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("{}", info);
     loop {
-        println!("Crash")
+        
     }
+    // loop {
+    //     println!("Crash")
+    // }
 }
 
 extern crate alloc;
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
+    
     // generator version: 0.3.1
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
+    
 
     esp_alloc::heap_allocator!(size: 72 * 1024);
 
@@ -84,8 +99,74 @@ async fn main(spawner: Spawner) {
     )
     .unwrap();
 
+    let pwr_btn_pin = peripherals.GPIO6;
     let ledc_pin = peripherals.LEDC;
     let backlight_pwm_pin = peripherals.GPIO5;
+    
+    let i2c_sda_pin = peripherals.GPIO11;
+    let i2c_scl_pin = peripherals.GPIO10;
+    let x = Output::new(i2c_sda_pin, Level::Low, OutputConfig::default().with_pull(Pull::None).with_drive_mode(DriveMode::OpenDrain));
+    // let mut x = Flex::new(i2c_sda_pin);
+    // x.pull_direction(Pull::Up);
+    // let mut y = Flex::new(i2c_scl_pin);
+    // y.pull_direction(Pull::Up);
+    let y = Output::new(i2c_scl_pin, Level::Low, OutputConfig::default().with_pull(Pull::None).with_drive_mode(DriveMode::OpenDrain));
+    
+    let frequency = Rate::from_khz(400);
+    let mut i2c = I2c::new(
+        peripherals.I2C0,
+        Config::default().with_frequency(frequency),
+    )
+    .unwrap()
+    .with_sda(x)
+    .with_scl(y);
+    
+    // println!("start");
+    
+    // for addr in 0..u8::MAX {
+    //     let result = i2c.write(addr, &[0, 4]);
+    //     if result.is_ok() || true {
+    //         println!("{:?}", result);
+    //     }
+    // }
+    // println!("end");
+    // loop {}
+    // i2c.write(PCF85063_ADDRESS, &[1, 0]);
+    
+    // let register = 0;
+    // let mut data = [0];
+    // i2c.write_read(PCF85063_ADDRESS, &[register], &mut data);
+    
+    // println!("response: {:?}", data);
+    
+    // .into_async();
+    
+    let mut rtc = PCF8563::new(i2c);
+    rtc.rtc_init().unwrap();
+    
+    let now = rtc.get_datetime().unwrap();
+    println!("Today is {}, {} {} 20{:02} {:02}:{:02}:{:02}\r", 
+                    weekday_name(now),
+                    now.day, 
+                    month_name(now),
+                    now.year, 
+                    now.hours, 
+                    now.minutes, 
+                    now.seconds
+    );
+
+    let config = InputConfig::default().with_pull(Pull::Up);
+    let mut button = Input::new(pwr_btn_pin, config);
+    
+    let mut was_pressed = false;
+    loop {
+        let is_pressed = button.is_low();
+        if is_pressed && !was_pressed {
+            println!("Button pressed!");
+        }
+        was_pressed = is_pressed;
+        Timer::after(Duration::from_millis(100)).await;
+    }
     
     let mut ledc: Ledc = Ledc::new(ledc_pin);
     
@@ -144,7 +225,7 @@ async fn main(spawner: Spawner) {
     println!("Before");
     
     {
-        display::Spd2010::new(spi).await;
+        // display::Spd2010::new(spi).await;
         // let mut xfsdffdv = display::Spd2010::new(spi).await;
         // xfsdffdv.init();
         // xfsdffdv.draw();
@@ -202,4 +283,43 @@ pub fn set_pin(i2c: &mut I2c<Async>, pin: u8, state: bool) {
     } else {
         println!("Parameter error, please enter the correct parameter!");
     }
+}
+
+// Helper function to get the correct name of the day
+fn weekday_name(datetime: DateTime) -> &'static str  {
+    let mut name = "Sunday";
+    let day = datetime.weekday;
+    match day {
+        0 => {name = "Sunday"}
+        1 => {name = "Monday"}
+        2 => {name = "Tuesday"}
+        3 => {name = "Wednesday"}
+        4 => {name = "Thursday"}
+        5 => {name = "Friday"}
+        6 => {name = "Saturday"}
+        _ => ()
+    }
+    name
+}
+
+// Helper function to get the correct name of the month
+fn month_name(datetime: DateTime) -> &'static str  {
+    let mut name = "January";
+    let month = datetime.month;
+    match month {
+        1 => {name = "January"}
+        2 => {name = "February"}
+        3 => {name = "March"}
+        4 => {name = "April"}
+        5 => {name = "May"}
+        6 => {name = "June"}
+        7 => {name = "July"}
+        8 => {name = "August"}
+        9 => {name = "September"}
+        10 => {name = "October"}
+        11 => {name = "November"}
+        12 => {name = "December"}        
+        _ => ()
+    }
+    name
 }
